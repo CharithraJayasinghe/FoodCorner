503baab7a3869a5cc18a750fd8e3b97c72eaf13a
“Charithra” <charithra@adtechnologies.lk>
2023-12-19 19:26:03 +0530

    Initial commit

---
+++ src/test/java/com/example/FoodCorner/FoodCornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FoodCorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodCornerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
